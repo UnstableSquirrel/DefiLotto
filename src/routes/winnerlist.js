9221cb656ba156eb06e337533cd8bd8b92d0c0c7
UnstableSquirrel <twnfut@gmail.com>
2022-11-23 23:36:05 +0100

    initial

--- src/routes/winnerlist.js
+++ src/routes/winnerlist.js
@@ -7,6 +7,6 @@ export const Winners =
     Prize: "100",
     Transaction: "0x8354b056eca474d7a695ab976e5a44750730eb73c1ec2f8bf1f73c59f6c6f897",
     Contest: "$1 Launch Mint",
-    Network: "Polygon137",
+    Network: "Polygon",
   },
 ]
